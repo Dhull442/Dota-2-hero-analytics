6858cb4b338fad79d5dfb794e45c590c4cc21fcc
Saksham Dhull <sakshamdhull442@gmail.com>
2021-03-25 17:47:37 +0530

    frontend

---
+++ frontend/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('templates/index.html')
+
+@app.route('/queries')
+def askquery():
+    data = result()
+    return render_template('templates/index.html',data = data)
+
+
+
+if __name__ == '__main__':
+   app.run()
