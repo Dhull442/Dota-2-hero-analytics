7ed521fe9d77a9fd34f24c527daf1727a1baaccc
Saksham Dhull <sakshamdhull442@gmail.com>
2021-03-28 10:32:32 +0530

    frontend changes

--- frontend/main.py
+++ frontend/main.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import psycopg2 as pg
+conn = pg.connect("dbname=project_db user=postgres password=password")
 
+# General Queries
+def query_q1():
+    # Top k heroes most selected
+
+
+
+
+
+    
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
-@app.route('/queries')
+@app.route('/query',methods=['POST'])
 def askquery():
-    data = result()
-    return render_template('templates/index.html',data = data)
+    query = request.form['query']
+    return render_template('index.html')
 
-
+@app.route('/update')
+def update():
+    return render_template('index.html')
 
 if __name__ == '__main__':
    app.run()
+   connect(
